555e5b96d27b55960e3b411d20842efb53426670
Jay Potter <j@ypotter.ca>
2024-03-29 10:01:44 -0400

    Create AbstractSession.php

---
+++ src/Potter/Session/AbstractSession.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\Session;
+
+abstract class AbstractSession implements SessionInterface
+{
+    abstract public function activate(): void;
+    abstract public function isActive(): bool;
+    abstract public function getId(): string;
+    abstract protected function setId(string $id): void;
+    abstract public function getName(): string;
+    abstract protected function setName(string $name): void;
+}
